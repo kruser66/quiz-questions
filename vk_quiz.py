a2674df1508a3c77ad6667d91c15819455f1d48f
kruser <kruser@yandex.ru>
2022-01-26 22:34:56 +0500

    Добавил Эхо-бот ВК

---
+++ vk_quiz.py
@@ -0,0 +1,36 @@
+import os
+import logging
+import vk_api as vk
+from vk_api.longpoll import VkLongPoll, VkEventType
+from dotenv import load_dotenv
+from random import randint
+
+
+logger = logging.getLogger('quiz-logger')
+
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=randint(1, 1000)
+    )
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    load_dotenv()
+
+    vk_token = os.environ['VK_GROUP_TOKEN']
+
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
